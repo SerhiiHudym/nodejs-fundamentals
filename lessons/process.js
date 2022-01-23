762adde48a244f78f42dc43ddc05677afeac8af2
Serhii <serhii.hudym@litslink.com>
2022-01-24 00:36:51 +0200

    First lesson. Added lesson for process global object.

---
+++ lessons/process.js
@@ -0,0 +1,17 @@
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// console.log(process.pid);
+
+// console.log(process.env);
+// console.log("PORT:", process.env.PORT);
+// console.log("Environment:", process.env.NODE_ENV);
+
+// console.log(process.argv);
+
+// if (Math.random() > 0.5) {
+//   while (true) {}
+// } else {
+//   console.log("До свидания.");
+//   process.exit();
+// }
